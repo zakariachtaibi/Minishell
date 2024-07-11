7107ea19b9059e7bae923ee2aa2a5ee74449e2cb
Huda Ch <80288208+houdach@users.noreply.github.com>
2024-07-11 14:35:34 +0000

    setting the env

--- srcs/main.c
+++ srcs/main.c
@@ -7,13 +7,11 @@ int main()
 
     while (1) 
     {
-        // Display the prompt and read input
         input = readline("minishell> ");
         if (!input) {
             break; 
         }
             add_history(input);
-        // printf("You entered: %s\n", input);
 
         free(input);
     }
