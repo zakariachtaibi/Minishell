f243222f9fb5ba08488cebe39df8f13de1894f32
Houda Chouai <hchouai@c4r5p5.1337.ma>
2024-07-18 15:02:55 +0100

    single quote

--- srcs/lexer/tokenizer.c
+++ srcs/lexer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: hchouai <hchouai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 14:10:15 by hchouai           #+#    #+#             */
-/*   Updated: 2024/07/18 14:14:22 by hchouai          ###   ########.fr       */
+/*   Updated: 2024/07/18 14:58:49 by hchouai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	has_unclosed_quotes(char *temp)
 	c = 0;
 	while (input[i])
 	{
-		if (input[i] == '"')
+		if (input[i] == '"' || input[i] == '\'' )
 			c++;
 		i++;
 	}
