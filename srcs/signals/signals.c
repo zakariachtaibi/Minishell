ea2f156bb7d0fb8556674f1ed5ca4a09239e2bf4
zchtaibi <zchtaibi@c4r2p8.1337.ma>
2024-11-10 21:37:52 +0100

    still error en qoutes

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,11 +6,22 @@
 /*   By: zchtaibi <zchtaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 16:36:08 by zchtaibi          #+#    #+#             */
-/*   Updated: 2024/11/10 21:22:58 by zchtaibi         ###   ########.fr       */
+/*   Updated: 2024/11/10 21:35:42 by zchtaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+void handle_sigint(int sig)
+{
+    if (sig == SIGINT)
+    {
+        printf("\n");
+        rl_on_new_line();
+        rl_replace_line("", 0);
+        rl_redisplay();
+    }
+}
 
 void sig_handler1(int test)
 {
